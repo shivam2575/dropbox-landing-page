5a284d2087d5d8d1d21b0a85594e33d29016eea6
shivam2575 <sk2575@gmail.com>
2025-05-23 13:45:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen text-blue-600">
+      <h1 className="text-3xl font-bold">DROPBOX CLONE</h1>
+    </div>
+  );
+}
+
+export default App;
